16de0fda320a65f6b32d418e3ad391e0b4d49d03
jzhang <jinping4038@gmail.com>
2025-08-25 10:08:08 -0400

    Initial Commit

---
+++ Marketplace/MarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceApp.swift
+//  Marketplace
+//
+//  Created by Jin Zhang on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarketplaceTests/MarketplaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceTests.swift
+//  MarketplaceTests
+//
+//  Created by Jin Zhang on 8/25/25.
+//
+
+import Testing
+@testable import Marketplace
+
+struct MarketplaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
